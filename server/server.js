f62dd99d410209686eee26c8032e20a48c74ddab
Kerim Özgiray <kerimozgiray@gmail.com>
2017-03-05 22:05:36 +0200

    Get By Id route added

--- server/server.js
+++ server/server.js
@@ -1,3 +1,7 @@
+const {
+  ObjectID
+} = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -39,6 +43,30 @@ app.get('/todos', (req, res) => {
       todos
     })
   }, (err) => {
+    res.status(400).send(err);
+  });
+
+});
+
+app.get('/todos/:id', (req, res) => {
+
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(400).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+
+    if (!todo) {
+      return res.send(404).send();
+    }
+
+    res.send({
+      todo
+    });
+
+  }).catch((err) => {
     res.status(400).send(err);
   });
 
