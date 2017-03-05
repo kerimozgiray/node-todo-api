f49da9ab324e3c144921584803d6da37d38d1d04
Kerim Özgiray <kerimozgiray@gmail.com>
2017-03-05 21:05:14 +0200

    Get/todos route added to api

--- server/server.js
+++ server/server.js
@@ -32,6 +32,18 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+
+  Todo.find().then((todos) => {
+    res.send({
+      todos
+    })
+  }, (err) => {
+    res.status(400).send(err);
+  });
+
+});
+
 app.listen(3000, () => {
   console.log('server running on localhost:3000');
 });
